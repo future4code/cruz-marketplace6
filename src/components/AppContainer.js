c4c6336827f6d593c76ea5dd5bbc7b44c68d63c6
dayknlang <dayaneknlang@gmail.com>
2021-03-31 19:02:13 -0300

    Criado acesso a API

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,11 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { createProduct } from "../api/api";
 
-export class AppContainer extends Component {
-  render() {
-    return (
-      <div>
-        <p>Pronto para começar!</p>
-      </div>
-    )
-  }
+export function AppContainer() {
+  const product1 = () => {
+    const newProduct = {
+      name: "Produto",
+      description: "Esse é um produto muito legal!",
+      price: 10,
+      paymentMethod: "card",
+      category: "Categoria 1",
+      photos: ["https://picsum.photos/300/200"],
+      installments: 3,
+    };
+    console.log("Teste");
+    createProduct(newProduct).then((res) => {
+      console.log(res);
+    });
+  };
+
+  return (
+    <div>
+      <p>Pronto para começar!</p>
+      <button onClick={product1}>Clique</button>
+    </div>
+  );
 }
